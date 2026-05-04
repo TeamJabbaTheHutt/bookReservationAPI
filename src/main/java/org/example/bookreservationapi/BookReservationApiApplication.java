e75bfa8feaee1d5447e551e0c64b7e5bd9a0b579
iotuz <theismillinger@gmail.com>
2026-05-04 10:20:33 +0200

    first

---
+++ src/main/java/org/example/bookreservationapi/BookReservationApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.bookreservationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookReservationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookReservationApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bookreservationapi/BookReservationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bookreservationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReservationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
