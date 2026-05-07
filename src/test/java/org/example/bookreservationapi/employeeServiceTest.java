ed825e5ec9e0e525802366d8def33a2b781d40b9
Andreas Holgersen <Andr9753@gmail.com>
2026-05-07 12:22:49 +0200

    Fixed everything

--- src/test/java/org/example/bookreservationapi/employeeServiceTest.java
+++ src/test/java/org/example/bookreservationapi/employeeServiceTest.java
@@ -5,42 +5,48 @@ import org.example.bookreservationapi.employee.repository.employeeRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
 
 
-private employeeRepository employeeRepository;
-private employeeService employeeService;
+public class EmployeeServiceTest() {
+
+    private employeeRepository employeeRepository;
+    private employeeService employeeService;
 
 
-@BeforeEach
-public void setUp() {
-    employeeRepository = mock(employeeRepository.class);
-    employeeService = new employeeService(employeeRepository)
+    @BeforeEach
+    public void setUp() {
+        employeeRepository = mock(employeeRepository.class);
+        employeeService = new employeeService(employeeRepository)
     }
 
-@Test
-void shouldFindEmployeeById() {
+    @Test
+    void shouldFindEmployeeById() {
 
-    employeeEntity employee = new employeeEntity();
+        employeeEntity employee = new employeeEntity();
 
-    employee.setId(1L);
-    employee.setEmployeeName("John");
-    employee.setRole("ADMIN");
+        employee.setId(1L);
+        employee.setEmployeeName("John");
+        employee.setRole("ADMIN");
 
-    when(employeeRepository.findById(1L))
-            .thenReturn(Optional.of(employee));
-
-
-    employeeDTO result = employeeService.findById(1);
+        when(employeeRepository.findById(1L))
+                .thenReturn(Optional.of(employee));
 
 
-    assertNotNull(result);
-    assertEquals("John", result.getEmployeeName());
-    assertEquals("ADMIN", result.getRole());
+        employeeDTO result = employeeService.findById(1);
 
-    verify(employeeRepository).findById(1L);
+
+        assertNotNull(result);
+        assertEquals("John", result.getEmployeeName());
+        assertEquals("ADMIN", result.getRole());
+
+        verify(employeeRepository).findById(1L);
+
+    }
 
 }
