643e7f5c777495e2b36a8f3386c57719981e7feb
Andreas Holgersen <Andr9753@gmail.com>
2026-05-08 10:21:53 +0200

    brug logged in user til at hente employee_id find bookinger baserert på employee_id (endpoint GET /api/reserviation/{id}) getbyEmployee filtrer i backend tilføj params for filter startDate og endDate

--- src/main/java/org/example/bookreservationapi/reservation/controller/ReservationController.java
+++ src/main/java/org/example/bookreservationapi/reservation/controller/ReservationController.java
@@ -1,15 +1,17 @@
 package org.example.bookreservationapi.reservation.controller;
 
-
-
+import org.example.bookreservationapi.employee.entity.EmployeeEntity;
 import org.example.bookreservationapi.reservation.entity.Reservation;
 import org.example.bookreservationapi.reservation.service.ReservationService;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -18,13 +20,29 @@ public class ReservationController {
 
     private final ReservationService reservationService;
 
+
     public ReservationController(ReservationService reservationService) {
         this.reservationService = reservationService;
     }
 
     @GetMapping
     public List<Reservation> getMyReservations(Authentication authentication) {
-        Long employeeId = Long.parseLong(authentication.getName());
-        return reservationService.getAllReservationByEmployeeId(employeeId);
+        String username = authentication.getName();
+         EmployeeEntity employee = reservationService.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("Employee not found: " + username));
+        return reservationService.getAllReservationByEmployeeId(employee.getId());
+    }
+
+    @GetMapping
+    public List<Reservation> getMyReservations(
+            Authentication authentication,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
+
+        String username = authentication.getName();
+        EmployeeEntity employee = reservationService.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("Employee not found: " + username));
+
+        return reservationService.getReservationsForEmployee(employee.getId(), startDate, endDate);
     }
 }
