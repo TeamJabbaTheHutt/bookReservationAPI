35589dda1d275f06b6a1cb1a6764d36cdf09eba1
Andreas Holgersen <Andr9753@gmail.com>
2026-05-13 10:29:57 +0200

    security config first impl

--- src/main/java/org/example/bookreservationapi/security/SecurityConfig.java
+++ src/main/java/org/example/bookreservationapi/security/SecurityConfig.java
@@ -7,17 +7,47 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-//
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
+    private final JwtAuthFilter jwtAuthFilter;
+
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // ingen csrf
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // ingen session
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/*.html", "/*.css", "/*.js").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
+
+
+/*
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -46,7 +76,7 @@ public class SecurityConfig {
 //                        .requestMatchers("/", "/*.html", "/*.css", "/*.js").permitAll()
 //
 //                        // Offentlige API-endpoints (booking-flow uden login)
-//                        .requestMatchers(HttpMethod.GET, "/api/employees", "/api/employees/*/treatments").permitAll()
+//                        .requestMatchers(HttpMethod.GET, "/api/employees", "/api/employees/treatments").permitAll()
 //                        .requestMatchers(HttpMethod.POST, "/api/reservations").permitAll()
 //
 //                        // Resten kræver login
@@ -86,4 +116,4 @@ public class SecurityConfig {
 //                    .build();
 //        };
 //    }
-//}
+//}*/
